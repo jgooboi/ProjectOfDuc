03e675147c9ee1bd5b70de65857ff5364b72de95
luong228 <90311663+luong228@users.noreply.github.com>
2021-11-28 23:49:01 +0700

    Modify Slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,19 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-autoSlides();
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slider-home");
-
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+// Automatic Slider
+var counter = 1;
+document.getElementById("radio1").checked = true;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 3) {
+        counter = 1;
     }
 
-    slides[slideIndex - 1].style.display = "block";
-}
-
-function autoSlides() {
-    var i;
-    var slides = document.getElementsByClassName("slider-home");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(autoSlides, 4000); // Change image every 2 seconds
-}
-
+}, 4000);
+// End automatic Slider
 var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function () {
+var x = setInterval(function() {
 
     // Get today's date and time
     var now = new Date().getTime();
