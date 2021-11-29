1eed57ab6d7ef13a62f673b97fb371f6cc981481
luong228 <90311663+luong228@users.noreply.github.com>
2021-11-30 01:05:51 +0700

    banner + embed video

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -3,23 +3,29 @@
 function openModal() {
     document.getElementById("myModal").style.display = "block";
 }
-
+document.getElementById("myModal").addEventListener('click', function(e) {
+    closeModal();
+});
+document.querySelector(".modal-content").addEventListener('click', function(e) {
+    e.stopPropagation()
+});
 // Close the Modal
 function closeModal() {
     document.getElementById("myModal").style.display = "none";
 }
 
-var slideIndex = 1;
-showSlidesImg(slideIndex);
+/* var slideImageIndex = 1;
+ showSlidesImg(slideImageIndex);
+ */
 
 // Next/previous controls
 function plusSlidesImg(n) {
-    showSlidesImg(slideIndex += n);
+    showSlidesImg(slideImageIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlideImg(n) {
-    showSlidesImg(slideIndex = n);
+    showSlidesImg(slideImageIndex = n);
 }
 
 function showSlidesImg(n) {
@@ -27,17 +33,53 @@ function showSlidesImg(n) {
     var slides = document.getElementsByClassName("slideImg");
     var dots = document.getElementsByClassName("demo");
     var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    if (n > slides.length) { slideImageIndex = 1 }
+    if (n < 1) { slideImageIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = dots[slideIndex - 1].alt;
+    slides[slideImageIndex - 1].style.display = "block";
+    dots[slideImageIndex - 1].className += " active";
+    captionText.innerHTML = dots[slideImageIndex - 1].alt;
 }
 
 // End Image Modal About Us
+
+// embed video
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Watch More Link click handlers
+    const $popup = document.querySelector('.video-popup');
+    const $modal = document.querySelector('#modal-video');
+    const $closeIcon = document.querySelector('.close-video');
+    const $watchMoreLink = document.querySelector('.banner-video__play-btn');
+    const $iframe = document.getElementById('iframe-video');
+
+    $watchMoreLink.addEventListener('click', function(e) {
+        $popup.style.display = 'block';
+        $modal.style.display = 'block';
+        e.preventDefault();
+    });
+    $closeIcon.addEventListener('click', function(e) {
+        $popup.style.display = 'none';
+        $modal.style.display = 'none';
+        $iframe.src = 'https://www.youtube.com/embed/UM1O-4ll-vU?autoplay=1';
+    });
+    // for escape key
+    document.addEventListener('keyup', function(e) {
+        if (e.key === "Escape") {
+            $popup.style.display = 'none';
+            $modal.style.display = 'none';
+            //Reload time iframe
+            $iframe.src = 'https://www.youtube.com/embed/UM1O-4ll-vU?autoplay=1';
+        }
+    });
+    // click outside of the popup, close it
+    $modal.addEventListener('click', function(e) {
+        $popup.style.display = 'none';
+        $modal.style.display = 'none';
+        $iframe.src = 'https://www.youtube.com/embed/UM1O-4ll-vU?autoplay=1';
+    });
+});
